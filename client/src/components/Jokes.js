7ebe21c818f71beb2ae295d17113a9065bab3cb4
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-05 09:01:38 +0100

    STRETCH: Create skeleton for components

---
+++ client/src/components/Jokes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Jokes = (props) => {
+  return (
+      <div>LIST OF JOKES</div>
+  )
+};
+
+export default Jokes;
---
+++ client/src/components/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignIn = (props) => {
+  return (
+      <div>SIGN IN</div>
+  )
+};
+
+export default SignIn;
