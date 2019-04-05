8d939b1c197c61f9762409350d84739ea97bcf04
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-05 09:15:21 +0100

    STRETCH: Add links to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { NavLink, Route, withRouter } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Jokes from './components/Jokes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav>
+          <NavLink to="/jokes">HOME</NavLink>
+          <NavLink to="/sign-in">SIGN IN</NavLink>
+          <NavLink to="/sign-up">SIGN UP</NavLink>
+          <button type="submit">SIGN OUT</button>
+        </nav>
 
       </div>
     );
