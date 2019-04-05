271ec141987e3a4850282e7526643747ca04a9b5
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-05 09:35:27 +0100

    STRETCH: Complete sign in functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import { NavLink, Route, withRouter } from 'react-router-dom';
+import axios from 'axios';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Jokes from './components/Jokes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signIn: {
+        username: '',
+        password: '',
+      },
+      signUp: {
+        username: '',
+        password: '',
+      },
+      jokes: [],
+      signedIn: false,
+    };
+  }
+
+  handleSignInChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      signIn: {...this.state.signIn, [name]: value}
+    });
+  };
+
+  handleSignIn = (event) => {
+    event.preventDefault();
+    axios
+        .post('http://localhost:3300/api/login', this.state.signIn)
+        .then(response => {
+          localStorage.setItem('token', response.data.token);
+          this.setState({ signedIn: true });
+          this.props.history.push('/jokes');
+        })
+        .catch((error) => console.log(error));
+  };
+
   render() {
+    const { signIn, signedIn } = this.state;
     return (
       <div className="App">
         <nav>
@@ -16,7 +53,15 @@ class App extends Component {
         </nav>
 
         <main>
-          <Route exact path="/sign-in" component={SignIn}/>
+          <Route exact path="/sign-in" render={(props) =>
+            <SignIn
+                {...props}
+                signIn={signIn}
+                signedIn={signedIn}
+                handleSignInChange={this.handleSignInChange}
+                handleSignIn={this.handleSignIn}
+            />}
+          />
           <Route path="/sign-up" component={SignUp}/>
           <Route path="/jokes" component={Jokes}/>
         </main>
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const SignIn = (props) => {
+  const { handleSignInChange, handleSignIn, signIn } = props;
   return (
-      <div>SIGN IN</div>
+    <div>
+      <form>
+        <h2>Sign In</h2>
+        <input
+          placeholder="Username..."
+          id="username"
+          name="username"
+          onChange={handleSignInChange}
+        />
+        <input
+            placeholder="Password..."
+            id="password"
+            name="password"
+            onChange={handleSignInChange}
+            type="password"
+        />
+        <button type="submit" onClick={(event) => handleSignIn(event, signIn)}>SIGN IN</button>
+      </form>
+    </div>
   )
 };
 
